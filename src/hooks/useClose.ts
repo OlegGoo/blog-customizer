09828a2ceba8ada281c2dec5ae5c063ef27213aa
Oleg Goloshumov <Oleggoloshumov@yandex.ru>
2024-08-24 23:53:58 +0300

    project-10

---
+++ src/hooks/useClose.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+
+type TUseClose = {
+	isOpen: boolean;
+	onclose: () => void;
+	rootRef: React.RefObject<HTMLElement>;
+};
+
+export const useClose = ({ isOpen, onclose, rootRef }: TUseClose) => {
+	useEffect(() => {
+		const handleOutsideClick = (event: MouseEvent) => {
+			const { target } = event;
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				isOpen && onclose();
+			}
+		};
+
+		const handleEsc = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				onclose();
+			}
+		};
+
+		document.addEventListener('mousedown', handleOutsideClick);
+		document.addEventListener('keydown', handleEsc);
+
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick);
+			document.removeEventListener('keydown', handleEsc);
+		};
+	}, [isOpen, onclose, rootRef]);
+};
